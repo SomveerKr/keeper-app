3dc382578ed9c484393ceb4d21831411da02ccc4
Somveer Kumar <7somveerkumar@gmail.com>
2022-09-29 11:22:54 +0530

    Keeper App V~0.1: Static layout using React

---
+++ src/components/Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Note() {
+    return(
+        <div className="note">
+            <h1>
+                Why Coding?
+            </h1>
+            <p>
+                Coding gives you power to gve life to your ideas.
+            </p>
+        </div>
+    )
+}
